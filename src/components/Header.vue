6c8bdf5e9715afc3b0ddf57593fe931d96c10c93
Esperanza Najera <esperanza.nave@gmail.com>
2023-07-13 23:01:48 -0700

    configurando proyecto vue, pinia, tailwind, router

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+   <div class="mx-auto container px-5 py-16">
+    <div>
+
+    </div>
+    <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+        <div class="space-y-4">
+             <label for="ingrediente" class="block text-white uppercase font-extrabold text-lg"> 
+                Nombre o Ingredientes
+             </label>
+             <input id="ingrediente"
+                    type="text"
+                    class="p-3 w-full rounded-lg focus:outline-none "
+                    placeholder="Nombre o Ingrediente: Ej. Vodka, Tequila, etc"
+            >
+        </div>
+
+        <div class="space-y-4">
+             <label for="categoria" class="block text-white uppercase font-extrabold text-lg"> 
+                Categoría
+             </label>
+             <select id="categoria"
+                    class="p-3 w-full rounded-lg focus:outline-none "
+                    placeholder="Nombre o Ingrediente: Ej. Vodka, Tequila, etc"
+            >
+            <option value="">--Seleccione--</option>
+             </select>
+        </div>
+        <input type="submit"
+               class="bg-orange-800 hover:bg-orange-900 cursor pointer text-white 
+                       font-extrabold w-full p-2
+                       rounded-lg uppercase"
+               value="Buscar Recetas" />
+
+    </form>
+   </div>
+  </header>
+</template>
