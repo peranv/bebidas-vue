21e1ddfed33f3c1c17936177802c5ddbb73b7e05
Esperanza Najera <esperanza.nave@gmail.com>
2023-07-13 23:22:23 -0700

    agregando navegacion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <script setup>
-
+   import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <header class="bg-slate-800">
    <div class="mx-auto container px-5 py-16">
-    <div>
-
+    <div class="flex justify-between items-center ">
+        <div>
+           <RouterLink :to="{name: 'inicio'}">
+               <img class="w-32" src="/img/logo.svg" alt="Logotipo" />
+           </RouterLink>
+        </div>
+        <nav class="flex gap-4">
+                <RouterLink :to="{name: 'inicio'}"
+                            class="text-white uppercase font-bold "
+                            active-class="text-orange-500"
+                >
+                        Inicio
+                </RouterLink>
+                <RouterLink :to="{name: 'favoritos'}"
+                            class="text-white uppercase font-bold "
+                            active-class="text-orange-500"
+                >
+                        Favoritos
+                </RouterLink>
+        </nav>
     </div>
     <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
         <div class="space-y-4">
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import InicioView from '../views/InicioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
- 
+      {
+        path:'/',
+        name:'inicio',
+        component: InicioView
+      },
+      {
+        path:'/favoritos',
+        name:'favoritos',
+        component: () => import('../views/FavoritosView.vue')
+      }
   ]
 })
 
