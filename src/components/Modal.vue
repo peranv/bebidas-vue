0925f9eb824e31187329dc37f46e0cec4df1642f
Esperanza Najera <esperanza.nave@gmail.com>
2023-07-21 11:22:12 -0400

    agregando las instrucciones a modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,6 +5,24 @@ import { useBebidasStore } from '../stores/bebidas';
 
 const modal = useModalStore()
 const bebidas = useBebidasStore()
+
+const formatearIngredientes = () => {
+    const ingredientesDiv = document.createElement('DIV')
+
+    for(let i = 1; i <= 15; i++)
+    {
+        if(bebidas.receta[`strIngredient${i}`]){
+            const ingrediente = bebidas.receta[`strIngredient${i}`]
+            const cantidad = bebidas.receta[`strMeasure${i}`]
+            const ingredienteCantidad = document.createElement('P')
+            ingredienteCantidad.classList.add('text-lg','text-gray-500')
+            ingredienteCantidad.textContent = `${ingrediente} - ${cantidad}`
+
+            ingredientesDiv.appendChild(ingredienteCantidad)
+        }
+    }
+    return ingredientesDiv
+}
 </script>
 
 <template>
@@ -25,7 +43,20 @@ const bebidas = useBebidasStore()
                       <img 
                          :src="bebidas.receta.strDrinkThumb"
                          :alt="'Imagen de ' + bebidas.receta.strDrink"
+                         class="mx-auto w-96"
                       />
+
+                      <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                          Ingredientes y Cantidades
+                      </DialogTitle>
+                      <div v-html="formatearIngredientes().outerHTML" >
+
+                      </div>
+
+                      <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                          Instrucciones
+                      </DialogTitle>
+                      <p class="text-lg text-gray-500">{{ bebidas.receta.strInstructions }}</p>
 
                   </div>
                 </div>
